5b80d21a485ec068a5d92ec6f547b3f5952ba795
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:28:33 +0200

    a

--- src/main/java/Ejercicio_5/UserAccount.java
+++ src/main/java/Ejercicio_5/UserAccount.java
@@ -25,4 +25,13 @@ public class UserAccount {
         this.following = new HashSet<>();
     }
 }
+
+public class Utils {
+    public static boolean isValidEmail(String email) {
+        return email.contains("@");
+    }
+
+    public static boolean isValidAlias(String alias) {
+        return alias.matches("[a-zA-Z0-9]+");
+    }
 }
