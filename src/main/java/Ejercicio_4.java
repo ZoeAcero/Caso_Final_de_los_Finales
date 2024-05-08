6df852f3d5dd03ff84ea94bcde66f2bacbff8455
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:02:00 +0200

    a

--- src/main/java/Ejercicio_4.java
+++ src/main/java/Ejercicio_4.java
@@ -1,4 +1,9 @@
 import java.util.*;
 
 public class Ejercicio_4 {
+    public static void main(String[] args) {
+        int number = 102;
+        int sum = sumOfDigits(number);
+        System.out.println(sum);
+    }
 }
