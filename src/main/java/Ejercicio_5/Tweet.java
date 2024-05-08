47b60664e7dcf9a43cde64cd25ca70d5b84a91a9
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:47:06 +0200

    ej 5 apartado 2

--- src/main/java/Ejercicio_5/Tweet.java
+++ src/main/java/Ejercicio_5/Tweet.java
@@ -6,4 +6,13 @@ public class Tweet {
     protected LocalDate time;
     protected String message;
     protected UserAccount sender;
+
+    public Tweet(LocalDate time, String message, UserAccount sender) {
+        if (message.length() > 140) {
+            throw new IllegalArgumentException("Message exceeds the maximum length of 140 characters");
+        }
+        this.time = time;
+        this.message = message;
+        this.sender = sender;
+    }
 }
