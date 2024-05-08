3797ad5e28f61b9d5529ee7e2822ce2637c8ffa1
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 11:03:42 +0200

    ej 5 apartado 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import Ejercicio_5.UserAccount;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<UserAccount> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("users.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                UserAccount user = UserAccount.fromString(line);
+                users.add(user);
+            }
 }
