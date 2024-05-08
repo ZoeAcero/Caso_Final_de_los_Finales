84f0114b297f0fcce81784fd82d58417d0013b1d
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:02:20 +0200

    a

--- src/main/java/Ejercicio_4.java
+++ src/main/java/Ejercicio_4.java
@@ -6,4 +6,12 @@ public class Ejercicio_4 {
         int sum = sumOfDigits(number);
         System.out.println(sum);
     }
+
+    public static int sumOfDigits(int n) {
+        if (n < 10) {
+            return n;
+        } else {
+            return n % 10 + sumOfDigits(n / 10);
+        }
+    }
 }
