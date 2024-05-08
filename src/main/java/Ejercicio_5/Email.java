284ccb6444564c43d2850d508b5977391ad288b6
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 11:57:00 +0200

    mejoras

--- src/main/java/Ejercicio_5/Email.java
+++ src/main/java/Ejercicio_5/Email.java
@@ -6,5 +6,12 @@ public class Email {
     private String subject;
     private String body;
 
+    public Email(String sender, String recipient, String subject, String body) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.subject = subject;
+        this.body = body;
+    }
+
     
 }
