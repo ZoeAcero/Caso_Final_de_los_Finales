ff44bd3a4b3f2bc8e13132abefd6ff06ef560f9d
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:50:42 +0200

    a

--- src/main/java/Ejercicio_5/DirectMessage.java
+++ src/main/java/Ejercicio_5/DirectMessage.java
@@ -1,5 +1,10 @@
 package Ejercicio_5;
 
 public class DirectMessage extends Tweet{
-    
+    private UserAccount receiver;
+
+    public DirectMessage(LocalDate time, String message, UserAccount sender, UserAccount receiver) {
+        super(time, message, sender);
+        this.receiver = receiver;
+    }
 }
