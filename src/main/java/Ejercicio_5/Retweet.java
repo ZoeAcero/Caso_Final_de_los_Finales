8505cd6c810a34a47cc18e9c1a8c63393679d179
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:49:15 +0200

    ej 5 apartado 2

--- src/main/java/Ejercicio_5/Retweet.java
+++ src/main/java/Ejercicio_5/Retweet.java
@@ -1,4 +1,10 @@
 package Ejercicio_5;
 
 public class Retweet extends Tweet {
+    private Tweet retweetedTweet;
+
+    public Retweet(LocalDate time, String message, UserAccount sender, Tweet retweetedTweet) {
+        super(time, message, sender);
+        this.retweetedTweet = retweetedTweet;
+    }
 }
