762acdfcce0d1c7756ad8aa5a2d528da9abbd34e
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 12:09:09 +0200

    mejoras

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void sortUsersByEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortUsersByTweetLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void postTweet() {
+    }
+}
