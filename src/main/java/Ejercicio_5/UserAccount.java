8b9a0f0c9207abcb9cd663c1756aaa90d717e315
ZoeAcero <zar.amazona@gmail.com>
2024-05-08 10:26:49 +0200

    a

--- src/main/java/Ejercicio_5/UserAccount.java
+++ src/main/java/Ejercicio_5/UserAccount.java
@@ -3,4 +3,12 @@ package Ejercicio_5;
 import java.util.*;
 
 public class UserAccount {
+    private String alias;
+    private String email;
+    private List<Tweet> tweets;
+    private List<Tweet> timeline;
+    private Set<UserAccount> followers;
+    private Set<UserAccount> following;
+
+    
 }
